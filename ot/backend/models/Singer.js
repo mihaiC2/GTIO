976f07fd3bad130054d1fb9dbd48f54a138de49c
JazminAM <jazminalmendras10@gmail.com>
2025-02-16 00:50:58 +0100

    Add description and age fields to Singer model; implement CRUD operations in singers route

--- ot/backend/models/Singer.js
+++ ot/backend/models/Singer.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const SingerSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    description: {type: String, required: false},
+    age: {type: Number, required: false},
     image: { type: String }, 
     votes: { type: Number, default: 0 } 
 });

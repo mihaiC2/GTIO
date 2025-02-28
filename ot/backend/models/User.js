3c7df4ccca8e8f04eb03e8ecd689faeec0f3595a
Roberto <gonzalez.134663@e.unavarra.es>
2025-02-28 20:41:51 +0100

    Refactor authentication to use Supabase; implement user registration, login, and token verification

--- ot/backend/models/User.js
+++ ot/backend/models/User.js
@@ -1,9 +1,91 @@
-const mongoose = require('mongoose');
+const { supabase } = require('../utils/supabase');
 
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-});
+const createUser = async (email, password, userData) => {
+    try {
+        const userId = await createAuthUser(email, password);
 
-module.exports = mongoose.model('User', UserSchema);
+        console.log({
+            id: userId,
+            email,
+            ...userData
+        })
+        await createDbUser({
+            id: userId,
+            email,
+            ...userData
+        });
+
+        return userId;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const createAuthUser = async (email, password) => {
+    try {
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password
+        });
+        if (error) throw error;
+        return data.user.id;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const createDbUser = async (userData) => {
+    try {
+        const { data, error } = await supabase
+            .from('users')
+            .insert(userData);
+        if (error) throw error;
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const authLogin = async (email, password) => {
+    try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        });
+        if (error) throw error;
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const getUserByAuthId = async (authId) => {
+    try {
+        const { data, error } = await supabase
+            .from('users')
+            .select('*')
+            .eq('id', authId)
+            .single();
+            
+        if (error) throw error;
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const updateUserByAuthId = async (authId, userData) => {
+    try {
+        const { data, error } = await supabase
+            .from('users')
+            .update(userData)
+            .eq('id', authId);
+        console.log(data)
+        if (error) throw error;
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = { createUser, authLogin, getUserByAuthId, updateUserByAuthId };

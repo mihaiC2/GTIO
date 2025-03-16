0d81e3a415da45d46837eba5b3e2d43ebd877092
yasin <yasinezziani@gmail.com>
2025-03-16 17:51:13 +0100

    tests for aut-service

--- ot/backend/user-service/src/server.ts
+++ ot/backend/user-service/src/server.ts
@@ -12,5 +12,5 @@ import userRoutes from './routes/users';
 
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));

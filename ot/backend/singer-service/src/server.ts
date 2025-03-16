f19488a6ab2fdf385c264c63cc518e144acf07b9
yasin <yasinezziani@gmail.com>
2025-03-16 17:51:13 +0100

    tests for aut-service

--- ot/backend/singer-service/src/server.ts
+++ ot/backend/singer-service/src/server.ts
@@ -13,5 +13,5 @@ import singersRoutes from './routes/singers';
 
 app.use('/api/singers', singersRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
--- ot/backend/vote-service/src/server.ts
+++ ot/backend/vote-service/src/server.ts
@@ -12,5 +12,5 @@ import voteRoutes from './routes/votes';
 
 app.use('/api/votes', voteRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));

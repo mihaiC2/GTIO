917d39bad0e4fdf3d680f7717f0f53894a288e73
nikol.1704 <nikol.lazarte@540deg.com>
2025-05-01 17:56:35 +0200

    add vote tests

--- ot/backend/vote-service/tests/__mocks__/@supabase/supabase-js.ts
+++ ot/backend/vote-service/tests/__mocks__/@supabase/supabase-js.ts
@@ -1,18 +1,36 @@
 const mockGetUser = jest.fn();
 const mockEq = jest.fn();
+const mockIn = jest.fn();
+const mockGte = jest.fn();
 const mockSingle = jest.fn();
+
 const mockSelect = jest.fn(() => ({
   eq: mockEq,
+  in: mockIn,
+  gte: mockGte,
   single: mockSingle,
 }));
 
-export { mockGetUser, mockEq, mockSingle, mockSelect };
+const mockInsert = jest.fn(() => ({
+  select: mockSelect,
+}));
+
+export {
+  mockGetUser,
+  mockEq,
+  mockIn,
+  mockGte,
+  mockSingle,
+  mockSelect,
+  mockInsert,
+};
 
 export const createClient = jest.fn().mockImplementation(() => ({
   auth: {
     getUser: mockGetUser,
   },
   from: jest.fn(() => ({
+    insert: mockInsert,
     select: mockSelect,
   })),
 }));

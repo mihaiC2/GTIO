4247696b5236caa6d5ee7e234e0e75e1a7f8f93d
nikol.1704 <nikol.lazarte@540deg.com>
2025-05-01 23:46:51 +0200

    fix scripts

--- ot/backend/scripts/tests.sh
+++ ot/backend/scripts/tests.sh
@@ -5,11 +5,11 @@ set -e # si falla algo el workflow se detiene
 services=("auth-service" "user-service" "singer-service" "vote-service")
 
 for service in "${services[@]}"; do
+  cd ..
   if [ -d "$service" ]; then
     cd "$service"
     echo "Ejecutando los tests de $service"
     npm run test
-    cd ..
   else
     echo "La carpeta $service no existe"
   fi

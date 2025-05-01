6303ef45212449220521dee1551770a526dcea7d
nikol.1704 <nikol.lazarte@540deg.com>
2025-05-01 23:32:41 +0200

    add workflow to tests

--- ot/backend/scripts/tests.sh
+++ ot/backend/scripts/tests.sh
@@ -4,21 +4,13 @@ set -e # si falla algo el workflow se detiene
 # Lista de carpetas de microservicios
 services=("auth-service" "user-service" "singer-service" "vote-service")
 
-echo "Iniciando instalación de dependencias..."
-
 for service in "${services[@]}"; do
   if [ -d "$service" ]; then
     cd "$service"
-    if [ -f package.json ]; then
-      echo "Ejecutando npm install en $service"
-      npm install
-    else
-      echo "No se encontró package.json en $service"
-    fi
+    echo "Ejecutando los tests de $service"
+    npm run test
     cd ..
   else
     echo "La carpeta $service no existe"
   fi
 done
-
-echo "Instalación completada!!"

34f3fb00d754460c21c17d818cd5b0e8dd03c96c
Fiuva <76161498+Fiuva@users.noreply.github.com>
2025-05-04 13:29:25 +0200

    - Updated logger configuration to include MongoDB transport for logging. - Modified logRequest function to include additional metadata in logs.

--- ot/backend/auth-service/src/utils/logger.ts
+++ ot/backend/auth-service/src/utils/logger.ts
@@ -2,6 +2,8 @@ import winston from 'winston';
 import path from 'path';
 import { Request } from 'express';
 
+import { MongoDB } from 'winston-mongodb';
+
 const customFormat = winston.format.combine(
     winston.format.colorize(),
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
@@ -12,22 +14,36 @@ const logger = winston.createLogger({
     level: 'info',
     format: customFormat,
     transports: [
-        //new winston.transports.Console(),
         new winston.transports.File({
             filename: path.join(__dirname, '../../logs/error.log'),
             level: 'error'
         }),
         new winston.transports.File({
-            filename: path.join(__dirname, '../../logs/error.log')
+            filename: path.join(__dirname, '../../logs/combined.log')
+        }),
+        new MongoDB({
+            db: process.env.MONGODB_URL as string,
+            collection: 'winston_logs',
+            options: { useUnifiedTopology: true },
+            level: 'info',
+            tryReconnect: true,
+            format: winston.format.metadata()
         })
     ]
 });
 
 export const logRequest = (req: Request, message: string, type: 'info' | 'error' = 'info') => {
     const logMessage = `[${req.method}] ${req.originalUrl} - ${message}`;
-    if (type === 'info') {
-        logger.info(logMessage);
-    } else if (type === 'error') {
-        logger.error(logMessage);
-    }
+    logger.log({
+        level: type,
+        message: logMessage,
+        metadata: {
+            method: req.method,
+            url: req.originalUrl,
+            ip: req.ip,
+            body: req.body
+        }
+    });
 };
+
+export default logger;

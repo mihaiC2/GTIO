b252b131cc54d65ef4a837e3304a7b4665cda5eb
Fiuva <76161498+Fiuva@users.noreply.github.com>
2025-05-10 18:05:00 +0200

    remove comments

--- ot/backend/auth-service/src/utils/logger.ts
+++ ot/backend/auth-service/src/utils/logger.ts
@@ -15,7 +15,6 @@ const logger = winston.createLogger({
         new MongoDB({
             db: process.env.MONGODB_URL || 'mongodb://localhost:27017',
             collection: 'winston_logs',
-            // options: { useUnifiedTopology: true },
             level: 'info',
             tryReconnect: true,
             format: winston.format.metadata(),
